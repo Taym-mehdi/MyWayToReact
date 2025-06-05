f349c64aa6787d26c61d778770e962b1e683d533
Tim <taym.mehdi@gmail.com>
2025-06-06 00:00:04 +0200

    adding CSS

--- mywaytoreact/src/App.js
+++ mywaytoreact/src/App.js
@@ -1,11 +1,15 @@
+import Navbar from './Navbar'
+import Home from './Home';
 
-import './App.css';
 
 function App() {
+  const title = "welcome to new Blog";
   return (
     <div className="App">
+      <Navbar />
      <div className="content">
-      <h1>App Component</h1>
+      <Home />
+     
      </div>
     </div>
   );
